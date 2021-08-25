aa099ea261e4f4b092bb56e788dc8195f7a71669
Jessicaruss99 <Jruss8@uncc.edu>
2021-08-25 19:52:28 -0400

    Created a simple nodejs server

--- week1.js
+++ week1.js
@@ -1,2 +1,11 @@
 //create a simple NodeJS "Hello World"
 console.log("Hello World");
+
+//create a simple NodeJS server
+var http = require ('http');
+var server = http.createServer(function (req, res){
+    res.writeHead(200);
+    res.end("Hello World 2");
+
+});
+server.listen(8080);
